990cec0d8d72f55c652d338482cd412d1fd95972
Aima Mughal <aimamughal1038@gmail.com>
2026-02-10 16:02:13 +0500

    backend

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(error => console.log(error));
@@ -17,6 +18,8 @@ const user = require('./routes/user');
 
 app.use('/api/v1/user', user);
 
-app.listen(PORT, () => {
-    console.log(`server is up on PORT:${PORT}`);
-})
+module.exports = app;
+
+// app.listen(PORT, () => {
+//     console.log(`server is up on PORT:${PORT}`);
+// })
