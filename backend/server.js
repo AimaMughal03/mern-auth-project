7669cf1ce6473b64c4d57e50e92a021ef502a794
Aima Mughal <aimamughal1038@gmail.com>
2026-02-13 08:44:38 +0500

    push

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const auth = require("./middleware/auth");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://mern-auth-project-frontend.vercel.app",
+    methods: ["GET, POST", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 
 mongoose.connect(process.env.MONGO_URI)
